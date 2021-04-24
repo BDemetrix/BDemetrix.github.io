66c51dc0ac67a2fb02e6dfd856a6a7fb3e3206d2
Bogdanov Dmitry <b.demetrix@gmail.com>
2021-04-24 09:55:23 +0300

    fixed

--- blocks/FlexTable/js/app.js
+++ blocks/FlexTable/js/app.js
@@ -1,8 +1,3 @@
-/* let boxInner = document.querySelectorAll('.box__inner');
-if (boxInner) {
-
-  boxInner.forEach( el => new SimpleBar(el))
-} */
 Array.prototype.forEach.call(
   document.querySelectorAll('.box__inner'),
   el => new SimpleBar(el)
