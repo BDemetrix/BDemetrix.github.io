eb709e0cf6a1ba0356a820606e1d5b4cb4bc040a
Bogdanov Dmitry <b.demetrix@gmail.com>
2021-05-21 13:36:26 +0300

    created a progress bar

---
+++ debag/smart-lab-profile/new/script.js
@@ -0,0 +1,42 @@
+  
+let circleBar = new ProgressBar.Circle('#upload-container', {   // #profile-avatar-link
+  color: "violet",
+  strokeWidth: 3.5,
+  trailWidth: 3.5,
+  trailColor: "transparent",
+  easing: "bounce",
+  from: { color: "#c3ff00"/* , width: 1 */ },
+  to: { color: "#02CA82"/* , width: 2  */},
+  text: {
+    value: '0',
+    className: 'progress-text',
+    style: {
+      //color: '#747474',
+      position: 'absolute',
+      /* left: '-10px',
+      bottom: '-10px', */
+      padding: 0,
+      margin: 0,
+      //transform: null
+    }
+  },
+  step: (state, shape) => {
+    shape.path.setAttribute("stroke", state.color);
+    shape.setText(Math.round(shape.value() * 100) + '%');
+    shape.text.style.color = state.color;
+  }
+});
+
+circleBar.animate(.90, {
+  duration: 1500
+});
+
+// progress-text-link
+
+let progressTextEls = document.querySelectorAll('.progress-text');
+console.log(progressTextEls);
+  progressTextEls.forEach( el => {
+  el.addEventListener('click', ()=>{
+    el.parentElement.querySelector('.progress-text-link').click();
+  })
+});
