ec78b346c59b9b07d0c3e404d6deab8c79fe4105
Bogdanov Dmitry <b.demetrix@gmail.com>
2021-06-27 23:00:40 +0300

    Очистка

--- smart-lab/test/files/adfox_HTML5.js
+++
@@ -1,8 +0,0 @@
-function callClick(n) {
-	var link = ( n ? flashVars.events[n] : flashVars.link );
-	window.open(flashVars.reference + "@" + link, flashVars.target);
-}
-
-function callEvent(n) {
-	(new Image()).src = flashVars.events[n]; 
-}
