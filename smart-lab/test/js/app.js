72feeb0d5e3a4f2cd5b17de3cc39a5b4b74b545c
Bogdanov Dmitry <b.demetrix@gmail.com>
2021-09-19 23:19:04 +0300

    fix

--- smart-lab/test/js/app.js
+++ smart-lab/test/js/app.js
@@ -469,7 +469,7 @@ function numOnly(event) {
 /**
  * Вычисление ширины центральной колонки content__main
  */
-/* const Content = document.querySelector('.content');
+const Content = document.querySelector('.content');
 const contentLeft = document.querySelector('.content__left');
 const contentMain = document.querySelector('.content__main');
 const contentRight = document.querySelector('.content__right');
@@ -489,7 +489,7 @@ function calcWidthContentMain() {
       contentMain.style.maxWidth = (contentWidth - contentLeftWidth - contentRightWidth) + 'px';
     }
   }
-} */
+}
 
 
 // Обработка событий для контекстного меню .context-menu
