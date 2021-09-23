19195909fca8455e1662c9283335a3cfd197ee3e
Bogdanov Dmitry <b.demetrix@gmail.com>
2021-09-23 23:15:32 +0300

    save

--- smart-lab/build/js/demo.js
+++ smart-lab/build/js/demo.js
@@ -11,4 +11,40 @@
       });
     });
   }
+
+  
+  const commentWriteBox = document.querySelector('.comment__write-box');
+  const commentTextarea = document.querySelector('.comment__textarea');
+  const commentSelectImg = document.querySelector('.comment__select-img');
+  const commentWriteImages = document.querySelector('.comment__write-images');
+
+  if (commentWriteBox && commentTextarea) {
+
+    commentTextarea.addEventListener('focus', () => {
+      commentWriteBox.classList.add('_open');
+    });
+
+    /* commentTextarea.addEventListener('blur', (e) => {
+      
+      if (e.target.closest('.comment__write-actions')) {
+        commentWriteBox.classList.remove('_open');
+      }
+    });
+
+    document.documentElement.addEventListener("click", (e) => {
+      console.log(e.target);
+      if (!e.target.closest('.comment__write-box')) {
+        //commentWriteBox.classList.remove('_open');
+      }
+    }); */
+
+    if (commentSelectImg && commentWriteImages) {
+      commentSelectImg.addEventListener('click', () => {
+        _slideToggle(commentWriteImages, 300);
+      });
+    }
+  }
+
 }());
+
+
