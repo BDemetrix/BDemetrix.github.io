ab46fb35d7e459234b256412e24225cbeb3814a1
Bogdanov Dmitry <b.demetrix@gmail.com>
2022-06-28 12:07:07 +0300

    fix

--- mobile_version/#src/js/files/sections-box.js
+++ mobile_version/#src/js/files/sections-box.js
@@ -5,10 +5,3 @@ const sectionsBoxSlider = new Swiper('.sections-box', {
     observer: true,
     resizeObserver: true,
 });
-
-const sectionsBoxSlidIndex = document.querySelector('.sections-box').dataset.active;
-console.log({
-    sectionsBoxSlidIndex
-})
-
-sectionsBoxSlider.activeIndex = sectionsBoxSlidIndex
