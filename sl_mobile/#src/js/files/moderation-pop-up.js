7e9c0f102b0a73c2e9f1fc5ee37cd78ac1cc34c0
Bogdanov Dmitry <b.demetrix@gmail.com>
2022-10-11 01:31:26 +0300

    Правки

--- sl_mobile/#src/js/files/moderation-pop-up.js
+++ sl_mobile/#src/js/files/moderation-pop-up.js
@@ -5,5 +5,27 @@
         window.addEventListener('close-custom-pop-up', () => {
             moderationPopUp.querySelector('._spoller._active').click();
         })
+
+        const inputs = moderationPopUp.querySelectorAll('textarea, input');
+        const support = (typeof window.IntersectionObserver === "function");
+
+        if (inputs && support) {
+            let observer = new IntersectionObserver((entries, observer) => {
+                entries.forEach(entry => {
+                    if (!entry.isIntersecting) {
+                        entry.element.scrollIntoView();
+                        console.log(entry)
+                    }
+                })
+            }); 
+
+            inputs.forEach(el => {
+                el.addEventListener('focus', () => {
+                    setTimeout(() => {
+                        observer.observe(el)
+                    }, 100);
+                })
+            });
+        }
     }
 }
