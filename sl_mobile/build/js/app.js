df4e403bd96bc479010905d11514d208b7ee51f5
Bogdanov Dmitry <b.demetrix@gmail.com>
2023-08-14 12:32:01 +0300

    Исправлен баг с превью новости

--- sl_mobile/build/js/app.js
+++ sl_mobile/build/js/app.js
@@ -929,7 +929,8 @@ function jBoxNotice(color, title, message) {
 // скрипт для перехода на страницу автора новости и отмену перехода к самой новости (в ленте новостей)
 
 (function () {
-    if (!document.querySelector('.js-news-feed-list')) return;
+    const newsList = document.querySelector('.js-news-feed-list')
+    if (!newsList) return;
 
     document.documentElement.addEventListener('click', e => {
         const newsAuthor = e.target.closest('.js-news-card-author');
@@ -938,6 +939,51 @@ function jBoxNotice(color, title, message) {
         e.preventDefault();
         window.location.href = newsAuthor.dataset.href;
     })
+
+    // пересчет ширин имени 
+    function calcAuthorWidthAll(newsList) {
+        let authorEls = newsList.querySelectorAll('.js-news-card-author');
+        authorEls.forEach(el => {
+            const parent = el.parentElement;
+            const dataEl = parent.querySelector('.js-news-card-date');
+            if (dataEl) {
+                const parentWidth = parent.offsetWidth;
+                const dataElWidth = dataEl.offsetWidth;
+                el.style.maxWidth = `${parentWidth - dataElWidth}px`;
+            }
+            else {
+               el.style.maxWidth = `100%`;
+            } 
+        });
+    }
+
+    calcAuthorWidthAll(newsList);
+    window.addEventListener('resize', () => {
+        calcAuthorWidthAll(newsList);
+    }, false);
+    window.addEventListener("orientationchange", () => {
+        calcAuthorWidthAll(newsList);
+    }, false);
+
+    // Изменение ширины при добавлении новых новостей
+    if (!window.hasOwnProperty('MutationObserver')) return;
+    let mutationObserver = new MutationObserver((mutations) => {
+        mutations.forEach(function (mutation) {
+            if (mutation.addedNodes.length) {
+                calcAuthorWidthAll(newsList)
+            }
+        });
+    });
+
+    mutationObserver.observe(newsList, {
+        attributes: true,
+        characterData: true,
+        childList: true,
+        subtree: true,
+        attributeOldValue: true,
+        characterDataOldValue: true
+    });
+
 }());
 ;(function($){
 	$.fn.datepicker.dates['ru'] = {
