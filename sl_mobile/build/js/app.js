dda2e6e3ed1e64e66b6e69f43e9c7502e403787e
BDemetrix <b.demetrix@gmail.com>
2023-10-22 18:03:14 +0300

    Апдейт для таблицы 'Рейтинг акций Мозговик'

--- sl_mobile/build/js/app.js
+++ sl_mobile/build/js/app.js
@@ -1393,3 +1393,23 @@ function jBoxNotice(color, title, message) {
 
 
 
+(function () {
+    // Подсказки в таблице котировок 
+    new jBox('Tooltip', {
+        theme: 'trades-table',
+        attach: '.js-trades-table-tooltip',
+        adjustPosition: true,
+        isolateScroll: false,
+        animation: "move", 
+		position: {
+			x: 'center',               // Horizontal position, use a number, 'left', 'right' or 'center'
+			y: 'bottom',               // Vertical position, use a number, 'top', 'bottom' or 'center'
+		},
+        offset: {                   
+            y: 10                  
+        },
+        maxWidth: 220,
+        pointer: false,
+        // trigger: 'click', 
+    });
+}());
